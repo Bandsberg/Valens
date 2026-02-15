a8b54fa6c859522d49e6f2eda1e9e143135df1b7
Bandsberg <rodekassen@bandsberg.dk>
2026-02-15 19:34:33 +0100

    init product page

---
+++ src/app/pages.rs
@@ -0,0 +1,5 @@
+mod product;
+pub use product::show_product;
+
+mod demo;
+pub use demo::show_demo;
---
+++ src/app/pages/product.rs
@@ -0,0 +1,6 @@
+use eframe::egui;
+
+pub fn show_product(ui: &mut egui::Ui) {
+    ui.heading("Product");
+    ui.label("This page has no local state (yet).");
+}
