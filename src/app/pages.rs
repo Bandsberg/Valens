fa9c68e8b484ac8f64e04643ad7c39b096483bc8
Bandsberg <rodekassen@bandsberg.dk>
2026-03-11 21:08:43 +0100

    rename customers to segments

--- src/app/pages.rs
+++ src/app/pages.rs
@@ -1,5 +1,5 @@
-mod customer;
-pub use customer::show_customer;
+mod segments;
+pub use segments::show_customer;
 
 pub mod product;
 pub use product::{ProductPage, product_sidepanel, show_product};
